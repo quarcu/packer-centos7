f54305b509cba218fae048a9519c8efb159de669
Piotr Szwarc <quarcu@gmail.com>
2015-11-20 11:42:41 +0100

    Install additional packages.

--- scripts/yum_update.sh
+++ scripts/yum_update.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 yum install -y epel-release
 yum clean all
+yum install -y gcc kernel-devel yum-priorities yum-plugin-fastestmirror yum-utils vim wget git htop nmon telnet
+yum -y groupinstall 'Development Tools'
 yum update -y
