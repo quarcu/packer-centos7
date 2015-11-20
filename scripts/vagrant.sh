6541f6bec87b676c9fa8d4ee5c8f18ea955574bb
Piotr Szwarc <quarcu@gmail.com>
2015-11-20 10:02:21 +0100

    Updated scripts/vagrant.sh.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir /home/vagrant/.ssh
-wget --no-check-certificate -O authorized_keys 'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
-mv authorized_keys /home/vagrant/.ssh
+# Vagrant specific
+date > /etc/vagrant_box_build_time
+
+# Installing vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+
+# Customize the message of the day
+echo 'Development Environment' > /etc/motd
